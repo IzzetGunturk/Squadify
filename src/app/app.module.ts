ba8729948280400ea84a39072042b132a3775095
IzzetGunturk <izzetgunturk2@hotmail.com>
2025-02-01 17:26:09 +0100

    Changed color of the UI + added spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ApplicationComponent } from './application/application.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationComponent
+    ApplicationComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
