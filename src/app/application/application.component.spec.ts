f326f5a5ea1aacccc86b634c0d090a3ec76bc39e
IzzetGunturk <izzetgunturk2@hotmail.com>
2025-06-07 14:16:55 +0200

    Added unit test

--- src/app/application/application.component.spec.ts
+++ src/app/application/application.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { ApplicationComponent } from './application.component';
+
+import { FormsModule } from '@angular/forms'; 
 
 describe('ApplicationComponent', () => {
   let component: ApplicationComponent;
@@ -8,7 +10,8 @@ describe('ApplicationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ApplicationComponent]
+      declarations: [ApplicationComponent],
+      imports: [FormsModule]
     })
     .compileComponents();
     
@@ -19,5 +22,97 @@ describe('ApplicationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('addPlayerToList()', () => {
+    it('should add a player to the list if name is provided', () => {
+      
+      //given
+      component.playerName = 'Cristiano Ronaldo'
+
+      // when
+      component.addPlayerToList();
+
+      //then
+      expect(component.playersList).toContain('Cristiano Ronaldo');
+      expect(component.playerName).toBe('');
+    });
+
+    it('should show error when playername is empty', () => {
+      
+      //given
+      component.playerName = ''
+
+      // when
+      component.addPlayerToList();
+
+      //then
+      expect(component.errorMessage).toBe('Fill in a name');
+    });
+  });
+
+  describe('deletePlayerFromList()', () => {
+    it('should delete player from list', () => {
+      
+      //given
+      component.playersList = ['Cristiano Ronaldo','Victor Osimhen'];
+
+      // when
+      component.deletePlayerFromList(1);
+
+      //then
+      expect(component.playersList).toEqual(['Cristiano Ronaldo']);
+    });
+  });
+
+  describe('generateTeams()', () => {
+    it('should show error when not enough players', () => {
+      
+      //given
+      component.playersList = ['Cristiano Ronaldo'];
+      component.selectedStandardNumberForTeams = 2;
+
+      // when
+      component.generateTeams();
+
+      //then
+      expect(component.errorMessage).toBe('Not enough players...');
+    });
+
+    it('should generate teams', fakeAsync(() => {
+      
+      //given
+      component.playersList = ['Cristiano Ronaldo', 'Victor Osimhen', 'Ousmane Dembele', 'Neymar Da Silva', 'Zlatan Ibrahimovic', 'Allan Saint-Maximin'];
+      component.selectedStandardNumberForTeams = 2;
+
+      // when
+      component.generateTeams();
+
+      //then
+      expect(component.loadingSpinner).toBeTrue();
+
+      tick(1000);
+
+      expect(component.generatedTeams.length).toBe(2);
+      expect(component.generatedTeams.flat().sort()).toEqual(['Cristiano Ronaldo', 'Victor Osimhen', 'Ousmane Dembele', 'Neymar Da Silva', 'Zlatan Ibrahimovic', 'Allan Saint-Maximin'].sort());
+      expect(component.loadingSpinner).toBeFalse();
+      expect(component.errorMessage).toBe('');
+    }));
+  });
+
+  describe('toggleDarkLightMode()', () => {
+    it('should toggle dark mode', () => {
+      
+      //given
+      spyOn(localStorage, 'setItem');
+      component.darkLightMode = false;
+
+      // when
+      component.toggleDarkLightMode();
+
+      //then
+      expect(component.darkLightMode).toBeTrue();
+      expect(localStorage.setItem).toHaveBeenCalledWith('darkLightMode', 'true');
+    });
   });
 });
